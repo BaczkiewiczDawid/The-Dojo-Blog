be0c1658cec62d1adeb3cab49a389cdbc1441bb5
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2021-07-24 19:56:44 +0200

    10-19

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,37 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-   const [name, setName] = useState('mario');
-   const [age, setAge] = useState(25);
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
-    const handleClick = () => {
-        setName('luigi');
-        setAge(30)
-    }
+    useEffect(() => {
+        setTimeout(() => {
+            fetch("http://localhost:8000/blogs")
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('could not fetch the data for that resource')
+                }
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setError(err.message);
+                setIsPending(false);
+            })
+        }, 700);
+    }, []);
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age }</p>
-            <button onClick={ handleClick }>Click me</button>
+            { error && <div>{ error }</div> }
+            { isPending && <div>Loading...</div> }
+           {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
      );
 }
